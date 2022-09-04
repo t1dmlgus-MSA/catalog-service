8733441d4bd6903ca4a4951dfc1e6034bce7c37c
t1dmlgus <dmlgusgngl@gmail.com>
2022-09-05 00:04:24 +0900

    init: catalog-service

---
+++ src/main/java/dev/t1dmlgus/catalogservice/CatalogServiceApplication.java
@@ -0,0 +1,15 @@
+package dev.t1dmlgus.catalogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class CatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/t1dmlgus/catalogservice/CatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.t1dmlgus.catalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
